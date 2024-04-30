29c869c9edb600526cff1657d798513bd498dffa
gilsongilsondefreitas2014 <gilsondefreitas2014@gmail.com>
2024-04-30 00:18:23 -0400

    Commit inicial

--- App.tsx
+++ App.tsx
@@ -3,18 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text key="1">Unisystem</Text>
+      <Text key="2">Gilson de Freitas</Text>
+      <StatusBar style="light" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
