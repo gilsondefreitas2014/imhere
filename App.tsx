d4c9bd0f25918297c2cd5ea4f82db6cca8bf24a6
Gilson de Freitas <92558731+gilsondefreitas2014@users.noreply.github.com>
2024-04-30 07:10:52 -0400

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,30 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24,
-    }}>
-      <Text style={{
-        color: '#FDFCFE',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
         Nome do evento
       </Text>
-
-      <Text style={{
-        color: '#6B6B6B',
-        fontSize: 16
-      }}>
+      <Text style={styles.eventDate}>
         Sexta, 4 de Novembro de 2022.
       </Text>
-
-      <StatusBar style="light" />
     </View>
   );
-} 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24,    
+  },
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16
+  }
+});
